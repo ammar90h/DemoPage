65bcdaaefe88156499e79a72f8c78abc1b186ce5
Ammar <ammar.gunner1@gmail.com>
2018-09-25 20:53:37 +0200

    Initial upload

---
+++ DemoPage/Database/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using DemoPage.Database.Config;
+using DemoPage.Database.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DemoPage.Database
+{
+    public class DataContext:DbContext
+    {   
+        //name of basa
+        public DataContext(): base("DemoBase")
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.Add(new CategoryConfig());
+            modelBuilder.Configurations.Add(new ArticleConfig());
+            modelBuilder.Configurations.Add(new SettingConfig());
+            modelBuilder.Configurations.Add(new UserConfig());
+
+        }
+    }
+}
